e4cfbe30b7dc4de0680f1d26209d3ea4b420952f
MariaEvdokimova <e.marrri@mail.ru>
2019-05-07 15:33:55 +0300

    начинаем оптимизировать векторную и растровую графику

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
+
+var link = document.querySelector(".button-modal");
+var popup = document.querySelector(".modal");
+
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
@@ -10,5 +14,19 @@ navToggle.addEventListener('click', function() {
     } else {
         navMain.classList.add('main-nav--closed');
         navMain.classList.remove('main-nav--opened');
+    }
+});
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+        }
     }
 });
